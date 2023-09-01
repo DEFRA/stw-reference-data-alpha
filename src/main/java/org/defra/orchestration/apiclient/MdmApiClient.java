56b6a9e262e07d02318d2cf06ed8457254caeca2
paul.blakeley <paulbla@kainos.com>
2023-09-01 15:38:05 +0100

    Tidy up.

--- src/main/java/org/defra/orchestration/apiclient/MdmApiClient.java
+++ src/main/java/org/defra/orchestration/apiclient/MdmApiClient.java
@@ -1,14 +1,7 @@
 package org.defra.orchestration.apiclient;
 
-import static com.toedter.spring.hateoas.jsonapi.MediaTypes.JSON_API;
-
-import java.util.List;
 import lombok.extern.slf4j.Slf4j;
-import org.defra.orchestration.apiclient.model.Commodity;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -23,23 +16,6 @@ public class MdmApiClient {
   @Autowired
   public MdmApiClient(WebClient mdmWebClient) {
     this.webClient = mdmWebClient;
-  }
-
-  public List<Commodity> getCommodities() {
-    CollectionModel<EntityModel<Commodity>> commodities = webClient.get()
-        .uri("/example")
-        .accept(JSON_API)
-        .retrieve()
-        .bodyToMono(new ParameterizedTypeReference<CollectionModel<EntityModel<Commodity>>>() {
-        })
-        .block();
-    if (commodities != null) {
-      return commodities.getContent().stream()
-          .map(EntityModel::getContent)
-          .toList();
-    } else {
-      return List.of();
-    }
   }
 
   public HttpStatusCode syncCommodityCodes() {

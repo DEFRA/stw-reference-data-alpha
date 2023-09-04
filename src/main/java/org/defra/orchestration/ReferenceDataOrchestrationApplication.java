80e088e3ae7946db8e5924230284915b877672fd
paul.blakeley <paulbla@kainos.com>
2023-09-04 09:46:41 +0100

    Tidy up

--- src/main/java/org/defra/orchestration/ReferenceDataOrchestrationApplication.java
+++ src/main/java/org/defra/orchestration/ReferenceDataOrchestrationApplication.java
@@ -4,11 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
-@EnableHypermediaSupport(type = {})
 public class ReferenceDataOrchestrationApplication {
 
   @Value("${mdm.baseUrl}")

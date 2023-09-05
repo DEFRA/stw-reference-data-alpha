f72034a4ffa261861ad0ed1af0772cccaa49e0e2
Reece Bennett <reeceb@kainos.com>
2023-09-05 16:49:10 +0100

    Change functions to not need authorization

--- src/main/java/org/defra/orchestration/function/IpaffsFunctions.java
+++ src/main/java/org/defra/orchestration/function/IpaffsFunctions.java
@@ -34,7 +34,7 @@ public class IpaffsFunctions {
       @HttpTrigger(
           name = "req",
           methods = HttpMethod.GET,
-          authLevel = AuthorizationLevel.FUNCTION
+          authLevel = AuthorizationLevel.ANONYMOUS
       ) HttpRequestMessage<Optional<String>> request,
       final ExecutionContext context) throws JsonProcessingException {
     log.info("certificates HTTP trigger starting");
@@ -49,7 +49,7 @@ public class IpaffsFunctions {
       @HttpTrigger(
           name = "req",
           methods = HttpMethod.GET,
-          authLevel = AuthorizationLevel.FUNCTION
+          authLevel = AuthorizationLevel.ANONYMOUS
       ) HttpRequestMessage<Optional<String>> request,
       final ExecutionContext context) throws JsonProcessingException {
     log.info("commodity-nomenclature HTTP trigger starting");
@@ -64,7 +64,7 @@ public class IpaffsFunctions {
       @HttpTrigger(
           name = "req",
           methods = HttpMethod.GET,
-          authLevel = AuthorizationLevel.FUNCTION
+          authLevel = AuthorizationLevel.ANONYMOUS
       ) HttpRequestMessage<Optional<String>> request,
       final ExecutionContext context) throws JsonProcessingException {
     log.info("certification-requirement HTTP trigger starting");
@@ -79,7 +79,7 @@ public class IpaffsFunctions {
       @HttpTrigger(
           name = "req",
           methods = HttpMethod.GET,
-          authLevel = AuthorizationLevel.FUNCTION
+          authLevel = AuthorizationLevel.ANONYMOUS
       ) HttpRequestMessage<Optional<String>> request,
       final ExecutionContext context) throws JsonProcessingException {
     log.info("species HTTP trigger starting");
@@ -94,7 +94,7 @@ public class IpaffsFunctions {
       @HttpTrigger(
           name = "req",
           methods = HttpMethod.GET,
-          authLevel = AuthorizationLevel.FUNCTION
+          authLevel = AuthorizationLevel.ANONYMOUS
       ) HttpRequestMessage<Optional<String>> request,
       final ExecutionContext context) throws JsonProcessingException {
     log.info("certification-nomenclature HTTP trigger starting");

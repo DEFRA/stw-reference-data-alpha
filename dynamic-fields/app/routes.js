8cbef01bb10d8d9b7983e64cffc23c42f662d8b1
Reece Bennett <reeceb@kainos.com>
2023-10-02 14:45:51 +0100

    Add first page

--- dynamic-fields/app/routes.js
+++ dynamic-fields/app/routes.js
@@ -6,4 +6,37 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
+const pages = [
+  {
+    id: 'contactDetails',
+    title: 'Contact details',
+    components: [
+      {
+        type: 'text',
+        name: 'name',
+        label: 'Name'
+      },
+      {
+        type: 'text',
+        name: 'email',
+        label: 'Email address'
+      },
+      {
+        type: 'text',
+        name: 'mobile',
+        label: 'Mobile number'
+      },
+      {
+        type: 'text',
+        name: 'agent',
+        label: 'Agent'
+      }
+    ]
+  }
+]
+
 // Add your routes here
+router.get('/contact-details', (req, res) => {
+  const { title, components } = pages[0]
+  res.render('questionPage', { pageTitle: title, components })
+})

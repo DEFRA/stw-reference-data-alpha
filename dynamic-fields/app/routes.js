8f883308577add79aa15cb763dd390498dcae4c3
Reece Bennett <reeceb@kainos.com>
2023-10-10 10:58:18 +0100

    Add commodity species page

--- dynamic-fields/app/routes.js
+++ dynamic-fields/app/routes.js
@@ -246,7 +246,53 @@ const pages = [
     ]
   },
   { title: 'Commodity picker' },
-  { title: 'Commodity species' },
+  {
+    url: '/commodity-species',
+    title: 'Commodity species',
+    nextPage: [
+      {
+        conditions: {
+          addAnother: ['yes']
+        },
+        url: '/commodity-picker'
+      },
+      {
+        url: '/purpose'
+      }
+    ],
+    components: [
+      {
+        type: 'select',
+        name: 'typeOfCommodity',
+        label: 'Type of commodity',
+        items: [ // We'd want this to be driven from reference data, not static
+          { value: 'domestic', text: 'Domestic' },
+          { value: 'farmed', text: 'Farmed game' },
+          { value: 'wild', text: 'Wild game' }
+        ]
+      },
+      {
+        type: 'checkbox',
+        name: 'speciesOfCommodity',
+        label: 'Select species of commodity',
+        hint: 'Select all that apply',
+        items: [
+          { value: 'Bison bison', text: 'Bison bison' },
+          { value: 'Bos taurus', text: 'Bos taurus' },
+          { value: 'Bubalus bubalis', text: 'Bubalus bubalis' }
+        ]
+      },
+      {
+        type: 'radio',
+        name: 'addAnother',
+        label: 'Do you want to add another commodity?',
+        items: [
+          { value: 'yes', text: 'Yes' },
+          { value: 'no', text: 'No' }
+        ]
+      }
+    ]
+  },
   { title: 'Variety and class' },
   { title: 'Commodity summary' },
   {

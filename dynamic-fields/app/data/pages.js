294a66d04c520d7e90a87e6d4ed08b09f401deaf
paul.blakeley <paulbla@kainos.com>
2023-10-12 11:10:10 +0100

    Added the products importing page

--- dynamic-fields/app/data/pages.js
+++ dynamic-fields/app/data/pages.js
@@ -852,8 +852,28 @@ module.exports = Object.freeze({
         { title: 'Route' },
         { title: 'Organisation address' },
         { title: 'Do you know the commodity code' },
-        { title: 'Which animal or product are you importing' },
-        { title: 'How many animals or products are you importing' },
+        {
+            title: 'Which animal or product are you importing?' ,
+            url: '/which-products-you-are-importing',
+            components: [
+                {
+                    type: 'details',
+                    label: 'What to do if your animal or product is not shown',
+                    hint: 'If your animal or product does not appear on this list then you will need to use the commodity selector <a href="/">here</a> . There is IPAFFS guidance available if you need further assistance.',
+                },
+                {
+                    type: 'select',
+                    name: 'productImporting',
+                    items: [
+                        { value: '', text: 'Please choose', default: true },
+                        { value: '61_188306', text: 'Bees – Bumble Bees' },
+                        { value: '60_8433', text: 'Bees - Honey Bees' },
+                        { value: '3_113311', text: 'Birds of Prey - Falcons' }
+                    ]
+                }
+            ]
+        },
+        { title: 'How many animals or products are you importing?' },
         { title: 'Commodity quantity' },
         { title: 'Enter ant identification details you have for the animal or product' },
         { title: 'When are you planning to import the animal or product' },

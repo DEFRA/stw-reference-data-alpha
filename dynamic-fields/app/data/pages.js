fbdb9cad659f514540f4902f04286d657c660fee
paul.blakeley <paulbla@kainos.com>
2023-10-17 14:54:17 +0100

    Added organisation Address page

--- dynamic-fields/app/data/pages.js
+++ dynamic-fields/app/data/pages.js
@@ -928,7 +928,45 @@ module.exports = Object.freeze({
       ]
     },
     {title: 'Route'},
-    {title: 'Organisation address'},
+    {
+      title: 'Organisation address',
+      url: '/organisation-address',
+      components: [
+        {
+          type: 'radio',
+          name: 'organisationAddress',
+          label: 'Organisation Address',
+          hint: 'Choose your organisation branch address',
+          items: [
+            {
+              value: 'cc7a4c6e-393b-4c22-b4bc-22534991baa0',
+              text: 'Animal and Plant Health Agency\n' +
+                'Woodham Lane\n' +
+                'New Haw\n' +
+                'Surrey\n' +
+                'Addlestone\n' +
+                'KT15 3NB\n' +
+                'United Kingdom of Great Britain and Northern Ireland'
+            },
+            {
+              value: '11fa26f4-0bbd-4b22-a31d-5a6cceb2bee7',
+              text: '960 Bergnaum Points\n' +
+                'Apt. 810\n' +
+                'Nevada\n' +
+                'West Miracle\n' +
+                '94533\n' +
+                'Brazil'
+            },
+            {
+              value: '7a9e132c-07e9-4d9f-8c99-3d142ccbaa2d',
+              text: 'address-line-1\n' +
+                'city-or-town\n' +
+                'Puerto Rico'
+            }
+          ]
+        }
+      ]
+    },
     {title: 'Do you know the commodity code'},
     {
       title: 'Which animal or product are you importing?',

be6b8ea89f80119af03c1c7a03137e42dc94626a
paul.blakeley <paulbla@kainos.com>
2023-10-19 13:27:13 +0100

    refactored to fit into our model!

--- dynamic-fields/app/data/pages.js
+++ dynamic-fields/app/data/pages.js
@@ -242,8 +242,66 @@ module.exports = Object.freeze({
       url: '/commodity-picker',
       nextPage: '/commodity-species',
       components: [{
-        type: 'commodityPicker',
-        name: 'commodity picker',
+        type: 'radio',
+        name: 'top-level',
+        label: 'Commodity',
+        hint: 'Find the commodity in the commodity tree',
+        items: [
+          {
+            value: '05',
+            html: '<b>05</b> - PRODUCTS OF ANIMAL ORIGIN, NOT ELSEWHERE SPECIFIED OR INCLUDED',
+          },
+          {
+            value: '07',
+            html: '<b>07</b> - EDIBLE VEGETABLES AND CERTAIN ROOTS AND TUBERS',
+            components: [
+              {
+                type: 'radio',
+                name: 'code07',
+                items: [{
+                  html: '<b>07 01</b> - Potatoes, fresh or chilled',
+                  value: '0701',
+                  components: [{
+                    type: 'radio',
+                    name: 'code0701',
+                    items: [
+                      {
+                        value: '07011000',
+                        html: '<b>07 01 10 00</b> - Seed'
+                      },
+                      {
+                        value: '070190',
+                        html: '<b>07 01 90</b> - Other',
+                        components: [{
+                          type: 'radio',
+                          name: 'code070190',
+                          items: [
+                            {
+                              value: '07019010',
+                              html: '<b>07 01 90 10</b> - For the manufacture of starch'
+                            },
+                            {
+                              value: '07019050',
+                              html: '<b>07 01 90 50</b> - New, from 1 January to 30 June'
+                            },
+                            {
+                              value: '07019090',
+                              html: '<b>07 01 90 90</b> - Other <b>(or select)</b>'
+                            }
+                          ]
+                        }]
+                      }
+                    ]
+                  }]
+                },
+                {
+                  value: '07020000',
+                  html: '<b>07 02 00 00</b> - Tomatoes, fresh or chilled <b>(or select)</b>'
+                }]
+              }
+            ]
+          }
+        ]
       }]
     },
     {
@@ -392,21 +450,20 @@ module.exports = Object.freeze({
               value: 'transhipment',
               components: [
                 {
-                  type: 'select',
-                  name: 'bcp',
-                  label: 'Exit border control post',
+                  type: 'radio',
+                  name: 'code07',
                   items: [{
                     text: 'Select border control post',
                     default: true
                   },
-                    {
-                      text: 'Belfast Pharmaceuticals - TESTY',
-                      value: 'TESTY'
-                    },
-                    {
-                      text: 'Edinburgh Airport (animals) - GBEDI4',
-                      value: 'GBEDI4'
-                    }]
+                  {
+                    text: 'Belfast Pharmaceuticals - TESTY',
+                    value: 'TESTY'
+                  },
+                  {
+                    text: 'Edinburgh Airport (animals) - GBEDI4',
+                    value: 'GBEDI4'
+                  }]
                 },
                 {
                   type: 'select',

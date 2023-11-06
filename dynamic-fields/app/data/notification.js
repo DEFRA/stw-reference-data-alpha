922b517fef0a08ce4942fd426291f0e89180fb31
paul.blakeley <paulbla@kainos.com>
2023-11-06 13:50:54 +0000

    Add more data to notification

--- dynamic-fields/app/data/notification.js
+++ dynamic-fields/app/data/notification.js
@@ -3,17 +3,21 @@ const updateNotification = req => {
   let data = req.session.data
   let notification = data.notification
 
-  if (!notification) {
-    notification = {}
+  if (!data.notification) {
+    notification = init()
   }
+
   switch(req.url) {
     case '/what-are-you-importing':
       notification.type = body.certificateType
       break
     case '/country-of-origin':
       notification.partOne = {
-        countryOfOrigin: body.countryOfOrigin,
-        consignedCountry: body.countryOfOrigin
+        ...notification.partOne,
+        commodities: {
+          countryOfOrigin: body.countryOfOrigin,
+          consignedCountry: body.countryOfOrigin
+        }
       }
       break
     case '/origin-of-the-import':
@@ -25,15 +29,75 @@ const updateNotification = req => {
       }
       break
     case '/commodity-picker':
-      notification.commodityCode = getCommodityCode(body)
+      notification = {
+        ...notification,
+        commodities: getCommodities(body)
+      }
       break
+  }
+  data.notification = notification
+}
+
+const init = () => {
+  return {
+    id: 999999,
+    referenceNumber: 'DRAFT.GB.2023.999999',
+    version: 0,
+    lastUpdated: new Date().toISOString(),
+    lastUpdatedBy: {
+      displayName: 'Mark Admin-Tester',
+      userId: '79f6dc68-2144-e911-a96a-000d3a29ba60'
+    },
+    partOne: {
+      personResponsible: {
+        name: 'Mark Admin-Tester',
+          companyId: '767ceb6a-2144-e911-a96c-000d3a29b5de',
+          companyName: 'Defra Test BIP',
+          country: 'GB',
+          tracesID: 1001,
+          phone: '020 8225 7295',
+          email: 'DefraTestBIP@anthunt3.33mail.com',
+          contactId: '79f6dc68-2144-e911-a96a-000d3a29ba60'
+      }
+    }
+  }
+}
+
+const getCommodities = body => {
+  return {
+    commodityComplement: [{
+      commodityID: getCommodityCode(body),
+      commodityDescription: getCommodityDescription(body),
+      complementID: 1,
+      speciesType: '2',
+      speciesClass: '14357'
+    }],
+    complementParameterSet: [{
+        uniqueComplementID: 'a288d00f-4f03-4f1f-b024-0fd72553b145',
+        complementID: 1,
+        keyDataPair: [
+          {}
+        ]
+    }],
+    countryOfOrigin: 'KH',
+    consignedCountry: 'ES-CN'
   }
 }
 
 const getCommodityCode = body => {
+  const commodity = getSelectedCommodity(body)
+  return commodity.split(' - ')[0]
+}
+
+const getCommodityDescription = body => {
+  const commodity = getSelectedCommodity(body)
+  return commodity.split(' - ')[1]
+}
+
+const getSelectedCommodity = body => {
   delete body.action
-  const largestKey= Object.keys(body).reduce((a, b) => {
-    return body[a].length > body[b].length ? a : b
+  const largestKey = Object.keys(body).reduce((a, b) => {
+    return body[a].split(' - ')[0].length > body[b].split(' - ')[0].length ? a : b
   })
   return body[largestKey]
 }
--- dynamic-fields/app/data/pages/commodity-picker.js
+++ dynamic-fields/app/data/pages/commodity-picker.js
@@ -9,44 +9,44 @@ module.exports = Object.freeze({
     hint: 'Find the commodity in the commodity tree',
     items: [
       {
-        value: '05',
+        value: '05 - PRODUCTS OF ANIMAL ORIGIN, NOT ELSEWHERE SPECIFIED OR INCLUDED',
         html: '<b>05</b> - PRODUCTS OF ANIMAL ORIGIN, NOT ELSEWHERE SPECIFIED OR INCLUDED'
       },
       {
-        value: '07',
+        value: '07 - EDIBLE VEGETABLES AND CERTAIN ROOTS AND TUBERS',
         html: '<b>07</b> - EDIBLE VEGETABLES AND CERTAIN ROOTS AND TUBERS',
         components: [
           {
             type: 'radio',
             name: 'code07',
             items: [{
+              value: '0701 - Potatoes, fresh or chilled',
               html: '<b>07 01</b> - Potatoes, fresh or chilled',
-              value: '0701',
               components: [{
                 type: 'radio',
                 name: 'code0701',
                 items: [
                   {
-                    value: '07011000',
+                    value: '07011000 - Seed',
                     html: '<b>07 01 10 00</b> - Seed'
                   },
                   {
-                    value: '070190',
+                    value: '070190 - Other',
                     html: '<b>07 01 90</b> - Other',
                     components: [{
                       type: 'radio',
                       name: 'code070190',
                       items: [
                         {
-                          value: '07019010',
+                          value: '07019010 - For the manufacture of starch',
                           html: '<b>07 01 90 10</b> - For the manufacture of starch'
                         },
                         {
-                          value: '07019050',
+                          value: '07019050 - New, from 1 January to 30 June',
                           html: '<b>07 01 90 50</b> - New, from 1 January to 30 June'
                         },
                         {
-                          value: '07019090',
+                          value: '07019090 - Other',
                           html: '<b>07 01 90 90</b> - Other <b>(or select)</b>'
                         }
                       ]
@@ -55,10 +55,10 @@ module.exports = Object.freeze({
                 ]
               }]
             },
-              {
-                value: '07020000',
-                html: '<b>07 02 00 00</b> - Tomatoes, fresh or chilled <b>(or select)</b>'
-              }]
+            {
+              value: '07020000 - Tomatoes, fresh or chilled',
+              html: '<b>07 02 00 00</b> - Tomatoes, fresh or chilled <b>(or select)</b>'
+            }]
           }
         ]
       }
